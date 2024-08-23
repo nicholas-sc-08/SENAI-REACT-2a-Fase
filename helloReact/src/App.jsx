c9892e36f20eb48dd7099ef575809574dca91e96
nicholas-sc-08 <nichola.reaper@gmail.com>
2024-08-23 17:14:26 -0300

    Arquivo atualizado

---
+++ helloReact/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <h1>Dinossauros</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus maiores in magnam perferendis eius esse rerum obcaecati sed facilis corporis quo minima impedit nesciunt blanditiis, ex ab ipsum exercitationem voluptas!</p>
+    </>
+  )
+}
+
+export default App
